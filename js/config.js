09aa5bb9232d760168615ed7b7033e934707fdfc
jab743 <jab743@nau.edu>
2016-01-10 12:44:08 -0700

    fixing

--- js/config.js
+++ js/config.js
@@ -8,7 +8,7 @@ var config = {
         //units: metric or imperial
         params: {
             q: 'Naperville, Illinois',
-            units: 'metric',
+            units: 'imperial',
             // if you want a different lang for the weather that what is set above, change it here
             lang: 'en',
             APPID: 'f15e45e2fe6f3ae3588524b4b6d3d857'
@@ -26,7 +26,7 @@ var config = {
         ],
         afternoon: [
             'Hello, beauty!',
-            'That looks nic on you!',
+            'That looks nice on you!',
             'Looking good today!',
 			'With your creative wit, Im sure you could come up with better compliments than me.'
         ],
@@ -43,8 +43,8 @@ var config = {
         //url: "https://p01-calendarws.icloud.com/ca/subscribe/1/n6x7Farxpt7m9S8bHg1TGArSj7J6kanm_2KEoJPL5YIAk3y70FpRo4GyWwO-6QfHSY5mXtHcRGVxYZUf7U3HPDOTG5x0qYnno1Zr_VuKH2M"
 	},
     news: {
-        //feed: '<rss version="2.0"><channel><generator>NFE/1.0</generator><title>Top Stories - Google News</title><link>http://news.google.com/news?hl=en&amp;pz=1&amp;ned=us</link><language>en-US</language><webMaster>news-feedback@google.com</webMaster><copyright>&amp;copy;2016 Google</copyright><pubDate>Sun, 10 Jan 2016 18:34:25 GMT</pubDate><lastBuildDate>Sun, 10 Jan 2016 18:34:25 GMT</lastBuildDate><image><title>Top Stories - Google News</title><url>https://ssl.gstatic.com/news-static/img/logo/en_us/news.gif</url><link>http://news.google.com/news?hl=en&amp;pz=1&amp;ned=us</link></image><description>Google News</description></channel></rss>'
+        feed: '<rss version="2.0"><channel><generator>NFE/1.0</generator><title>Top Stories - Google News</title><link>http://news.google.com/news?hl=en&amp;pz=1&amp;ned=us</link><language>en-US</language><webMaster>news-feedback@google.com</webMaster><copyright>&amp;copy;2016 Google</copyright><pubDate>Sun, 10 Jan 2016 18:34:25 GMT</pubDate><lastBuildDate>Sun, 10 Jan 2016 18:34:25 GMT</lastBuildDate><image><title>Top Stories - Google News</title><url>https://ssl.gstatic.com/news-static/img/logo/en_us/news.gif</url><link>http://news.google.com/news?hl=en&amp;pz=1&amp;ned=us</link></image><description>Google News</description></channel></rss>'
         //feed: 'http://www.nytimes.com/services/xml/rss/nyt/HomePage.xml'
-		feed: 'http://news.google.com/news?hl=en&amp;pz=1&amp;ned=us'
+		//feed: 'http://news.google.com/news?hl=en&amp;pz=1&amp;ned=us'
 	}
 }
