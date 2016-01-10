fdd7ce985b2e2ec55fb1a52144300206ac7ac21a
jab743 <jab743@nau.edu>
2016-01-10 13:58:04 -0700

    more complaments

--- js/config.js
+++ js/config.js
@@ -22,19 +22,100 @@ var config = {
             'Enjoy your day!',
             'You Rock!',
 			'I like your style',
-			'You get an A+!'
+			'You get an A+!',
+			'You are somebodys reason to smile',
+			'Smile and the world smiles with you',
+			'Hope you always find a reason to smile',
+			'Its a beautiful day - do not let it get away',
+			'Be brave',
+			'Be happy today',
+			'I like your jacket',
+			'You are like sunshine on a rainy day',
+			'Your hair looks stunning',
+			'God has great plans for you',
+			'I am on top of the world',
+			'Reach for your dreams',
+			'Soar high today',
+			'You are a bucket of awesome',
+			'Do not worry, you will do great',
+			'You are like a snowflake - you are unique',
+			'Always do your best',
+			'The secret of getting ahead is getting started'
+			
+			
+			
+			
+			
+			
+			
+			
         ],
         afternoon: [
             'Hello, beauty!',
             'That looks nice on you!',
             'Looking good today!',
-			'With your creative wit, Im sure you could come up with better compliments than me.'
+			'With your creative wit, Im sure you could come up with better compliments than me',
+			'In a world where you can be anything be yourself',
+			'Be yourself - theres no one better',
+			'What makes  you different is what makes you beautiful',
+			'I like your shoes',
+			'Dare whoever is reading this - smile ',
+			'why fit in when you were born to stand out',
+			'never let fear of what others think stop you from being yourself',
+			'Good effort',
+			'well done',
+			'Way to go!',
+			'People behind you at movies think you are the eprfect hit',
+			'You are a fountain of inspiration',
+			'Your handshake conveys intelligence, confidence, and minor calmness',
+			'Hello good looking',
+			'Take a break; you have earned it',
+			'All of your ideas are brilliant',
+			'You are a star of my daydreams',
+			'I am having trouble coming up with a compliment worthy of you',
+			'Your mind is a maze of amasement',
+			'You are the wind beneath my wings',
+			'Your shoes are untied but for you its cool',
+			'Oh, I can keep going',
+			'You know you can',
+			'You are even better than sparkles and unicorns combined',
+			'I am enchanted to meet you',
+			'well played',
+			'You intrigue me',
+			'Your smile could illuminate the depths of the ocean'
+	
+			
+			
+			
+			
+			
         ],
         evening: [
             'Way to go!',
             'You look nice!',
             'Well Done!',
 			'All of your ideas are brilliant!',
+			'Never give up on your dreams',
+			'Give thanks',
+			'for every dark night there is a brighter day',
+			'I love you',
+			'Dream',
+			'You are spontaneous and I love it',
+			'You are a bucket of awesome',
+			'You listen to the coolest music',
+			'Your turn to cook',
+			'Dessert time!',
+			'I like your style!',
+			'well done',
+			'Way to go!',
+			'People behind you at movies think you are the eprfect hit',
+			'You are a fountain of inspiration',
+			'Your handshake conveys intelligence, confidence, and minor calmness',
+			'Hello good looking',
+			'Take a break; you have earned it'
+			
+			
+		
         ]
     },
     calendar: {
